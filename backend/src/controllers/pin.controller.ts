e4824ea0f21be82ff3334c56a5ed9c005a06d4b4
Guillaume Sere <guillaumesere@hotmail.fr>
2022-09-21 16:00:50 +0200

    modif function getpin

--- backend/src/controllers/pin.controller.ts
+++ backend/src/controllers/pin.controller.ts
@@ -1,29 +1,53 @@
 import { Request, Response } from "express";
 import { Pin } from "../models/pin.model";
-import * as express from "express";
 
-const router = express.Router();
 
-// create pin
-const addPin = router.post("/", async (req: Request, res: Response) => {
-    const newPin = new Pin(req.body);
-    try {
-      const savedPin = await newPin.save();
-      res.status(200).json(savedPin);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+const addPin = (req: Request, res: Response) => {
+    const newPin = new Pin({
+        username: req.body.username,
+        title: req.body.title,
+        desc: req.body.desc,
+        rating: req.body.rating,
+        long: req.body.long,
+        lat: req.body.lat,
+    });
+    newPin.save((err, pin) => {
+        if (err) {
+            res.status(500).send({ message: err});
+        }else{
+            if (pin) {
+                res.status(200).send({
+                    username: pin.username,
+                    title: pin.title,
+                    desc: pin.desc,
+                    rating: pin.rating,
+                    long: pin.long,
+                    lat: pin.lat
+                });
+            };
+        }
+    }) 
+};
 
-  // get all pins
-const getPin = router.get("/", async (req: Request, res: Response) => {
-    try {
-      const pins = await Pin.find();
-      res.status(200).json(pins);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+const getPin = (req: Request, res: Response) => {
+    const allPin = Pin.find({
+        username: req.body.username,
+        title: req.body.title,
+        desc: req.body.desc,
+        rating: req.body.rating,
+        long: req.body.long,
+        lat: req.body.lat,
+    });
+    allPin.find((err, pin) => {
+        if (err) {
+            res.status(500).send({message: err});
+        }else{
+            if (pin) {
+                res.status(200).send();
+            };
+        }
+    })
+};
 
 export const controller = {
     addPin,
